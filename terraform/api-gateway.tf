0312c866b4d0aebffd2718ea03d10a833c1ab6c4
Jubel Cassio <jubelcassioverridasilva@gmail.com>
2024-09-28 20:32:49 +0000

    feat: :bricks: Updated api gateway config and rds endpoint url

--- terraform/api-gateway.tf
+++ terraform/api-gateway.tf
@@ -17,6 +17,14 @@ resource "aws_apigatewayv2_stage" "swagger-ui" {
   auto_deploy = true
 }
 
+
+resource "aws_apigatewayv2_stage" "swagger-v3" {
+  api_id = aws_apigatewayv2_api.main.id
+
+  name        = "v3"
+  auto_deploy = true
+}
+
 resource "aws_apigatewayv2_stage" "v1" {
   api_id = aws_apigatewayv2_api.main.id
 
@@ -66,11 +74,17 @@ data "aws_lb" "java-app-service" {
         "kubernetes.io/service-name": "default/java-app-service"
     }
 }
+
+
+data "aws_lb_listener" "java-app-service" {
+  load_balancer_arn = data.aws_lb.java-app-service.arn
+  port = 8080
+}
+
 resource "aws_apigatewayv2_integration" "eks" {
   api_id = aws_apigatewayv2_api.main.id
 
-#   integration_uri    = "arn:aws:elasticloadbalancing:us-east-1:105833672688:listener/net/a85b7efaa5950471facb7d0b8e0e96bb/efa74dfcb600a49b/c6cb48fdcfdb7556"
-  integration_uri = "arn:aws:elasticloadbalancing:us-east-1:105833672688:listener/net/a131fd50ed7d0493cae86b8913f43198/78c05c8f3df617e4/7cc354859a5bd697"
+  integration_uri = data.aws_lb_listener.java-app-service.arn
   integration_type   = "HTTP_PROXY"
   integration_method = "ANY"
   connection_type    = "VPC_LINK"
